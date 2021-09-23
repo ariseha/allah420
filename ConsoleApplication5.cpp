49312884bae408827f117b164d96c66d320508b0
ariseha <91275299+ariseha@users.noreply.github.com>
2021-09-23 17:43:38 +0200

    Add files via upload

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int GodRodjenja, Starost, Mjeseci;
+	int TrenutnaGodina;
+	int Mjesec;
+	cout << "Koje ste godine rodjeni? " << endl;
+	cin >> GodRodjenja;
+	cout << "Koja je trenutna godina? " << endl;
+	cin >> TrenutnaGodina;
+	Starost = TrenutnaGodina - GodRodjenja;
+	Mjeseci = Starost * Mjesec;
+	cout << "Trenutno imate " << Starost <<  endl;
+	return 0;
+}
+
+
+
